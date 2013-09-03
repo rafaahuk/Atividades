bd789fb1d478f2fcbe6cbcd976db658b2459fe5a
Rafael Oliveira <rafaahuk@gmail.com>
2013-09-03 08:33:40 -0300

    Jogo educativos sendo enviado para git

---
+++ Mario/Jogo1/Jogo1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Jogo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(986, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 44);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(986, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 66);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Jogo1.Properties.Resources.Cavalo;
+            this.pictureBox4.Location = new System.Drawing.Point(1013, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(242, 118);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Jogo1.Properties.Resources.princesa;
+            this.pictureBox3.Location = new System.Drawing.Point(828, 315);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 70);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Jogo1.Properties.Resources.mario1;
+            this.pictureBox2.Location = new System.Drawing.Point(77, 516);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(59, 58);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Jogo1.Properties.Resources._132975_Papel_de_Parede_Mario_World__132975_1440x900;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(925, 601);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1133, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 66);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "SAIR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1277, 586);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Mario/Jogo1/Jogo1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Jogo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "CAVALO")
+            {
+                for (int I = 0; I <= 100; I++)
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X + 1, pictureBox2.Location.Y);
+                    pictureBox4.Image = Properties.Resources.Panda;
+                    textBox1.Clear();
+                    textBox1.Focus();
+                }
+
+            }
+            if (textBox1.Text == "PANDA")
+            {
+                for (int I = 0; I <= 100; I++)
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X + 1, pictureBox2.Location.Y);
+                    pictureBox4.Image = Properties.Resources.vaca;
+                    textBox1.Clear();
+                    textBox1.Focus();
+                }
+            }
+            if (textBox1.Text == "VACA")
+            {
+                for (int I = 0; I <= 100; I++)
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X + 1, pictureBox2.Location.Y);
+                    pictureBox4.Image = Properties.Resources.elefante;
+                    textBox1.Clear();
+                    textBox1.Focus();
+                }
+            }
+
+            if (textBox1.Text == "ELEFANTE")
+            {
+                for (int I = 0; I <= 100; I++)
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X + 1, pictureBox2.Location.Y);
+                    pictureBox4.Image = Properties.Resources.sapo;
+                    textBox1.Clear();
+                    textBox1.Focus();
+                }
+            }
+            if (textBox1.Text == "SAPO")
+            {
+                for (int I = 0; I <= 100; I++)
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X + 1, pictureBox2.Location.Y);
+                    pictureBox4.Image = Properties.Resources.Leao;
+                    textBox1.Clear();
+                    textBox1.Focus();
+                }
+            }
+            if (textBox1.Text == "LEAO")
+            {
+                for (int I = 0; I <= 55; I++)
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - 1);
+                    pictureBox4.Image = Properties.Resources.Gato;
+                    textBox1.Clear();
+                    textBox1.Focus();
+                }
+            }
+
+            if (textBox1.Text == "GATO")
+            {
+                for (int I = 0; I <= 120; I++)
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X + 1, pictureBox2.Location.Y);
+                    pictureBox4.Image = Properties.Resources.cobra;
+                    textBox1.Clear();
+                    textBox1.Focus();
+                }
+            }
+
+            if (textBox1.Text == "COBRA")
+            {
+                for (int I = 0; I <= 25; I++)
+                {
+                    pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - 1);
+
+                    for (int J = 0; J <= 5; J++)
+
+                        pictureBox2.Location = new Point(pictureBox2.Location.X + 1, pictureBox2.Location.Y);
+
+
+                    pictureBox4.Image = Properties.Resources.Coelho;
+                    textBox1.Clear();
+                    textBox1.Focus();
+                }
+            }
+            if (textBox1.Text == "COELHO")
+            {
+                for (int I = 0; I <= 30; I++)
+                {
+
+                    pictureBox2.Location = new Point(pictureBox2.Location.X - 1, pictureBox2.Location.Y);
+
+                    for (int J = 0; J <= 3; J++)
+                    {
+                        pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - 1);
+
+                    }
+                }
+                pictureBox2.Image = Properties.Resources._400_694_Mario_e_Princesa_Peach;
+
+                pictureBox3.Visible = false;
+                textBox1.Clear();
+
+                
+                Form2 form = new Form2();
+                form.Show();
+                this.Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("AJUDE O MARIO A CHEGAR NA PRINCESA , COLOQUE O NOME DO ANIMAL QUE APARECE NA FIGURA E DE OK");
+            SoundPlayer fundo = new SoundPlayer(@"C:\\Mario.wav");
+            fundo.Play();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        }
+    }
+
